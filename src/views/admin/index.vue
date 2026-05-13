577f1806dcfdafe783e231e1528fea57b9604741
plain <2946082737@qq.com>
2026-05-13 21:05:29 +0800

    feat: 更新品牌名称为“智颐安联”，修改相关组件中的logo和标题

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -5,7 +5,7 @@
         <!-- logo -->
         <div class="header-logo">
           <img src="/imgs/logo.png" alt="Logo">
-          <span>智护银龄</span>
+          <span>智颐安联</span>
         </div>
         <el-button style="padding: 24px; font-size: 16px" type="danger" size="large"
           @click="$router.push('/monitor')">进入监控界面</el-button>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -10,7 +10,7 @@
       </div>
 
       <div class="brand-content">
-        <h1 class="floating-text">智护银龄</h1>
+        <h1 class="floating-text">智颐安联</h1>
         <p class="fade-in-text">让科技更有温度，让晚年更自在</p>
       </div>
     </div>
@@ -38,6 +38,7 @@
 }
 
 @keyframes float {
+
   0%,
   100% {
     transform: translateY(0);
